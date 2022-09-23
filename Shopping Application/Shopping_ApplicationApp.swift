39d32960ba53b01a64721459b0fb9a2e27cca4b5
Bradlee King <Kingofbrad@gmail.com>
2022-09-23 12:30:21 +0100

    Initial Commit

---
+++ Shopping Application/Shopping_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_ApplicationApp.swift
+//  Shopping Application
+//
+//  Created by Bradlee King on 23/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
